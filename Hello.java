d0a1309d36a6acb47dc6c49ab3c22e8350d07ebc
AndrwYan <beyond1997110@gmail.com>
2021-01-30 17:51:15 +0800

    First Commit

---
+++ Hello.java
@@ -0,0 +1,65 @@
+//每k个一组反转链表
+//思路：头插法
+
+public class Solution{
+	public ListNode ReverseKGroup(ListNode head,int k) {
+		if(Head==null||head.next==null||k==1) return head;
+		ListNode res=new ListNode(0);
+		res.next=head;
+		int length=0;
+
+		while(head!=null){
+			length++;
+			head=head.next;
+		}
+		for(){
+			for(){
+			}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
